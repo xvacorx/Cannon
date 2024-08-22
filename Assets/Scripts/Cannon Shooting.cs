75b324736455f15b3241c1010216567c0624a8e4
xvacorx <theproofthevictors@gmail.com>
2024-08-22 17:04:35 -0300

    Cannon System done

--- Assets/Scripts/Cannon Shooting.cs
+++ Assets/Scripts/Cannon Shooting.cs
@@ -1,15 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CannonShooting : MonoBehaviour
 {
+    public Slider rotationYSlider;  // Slider para rotar el cañón en Y (horizontal)
+    public Slider rotationXSlider;  // Slider para rotar el cañón en X (vertical)
+    public Slider powerSlider;      // Slider para controlar la potencia del disparo
+
+    [SerializeField] Rigidbody projectilePrefab;
+    [SerializeField] Transform firePoint;
+
+    private float attackSpeed = 1f;
+    private float nextAttackTime;
+
     private void Update()
     {
-        Shoot();
+        Rotation();
+        if (Input.GetButtonDown("Jump") && Time.time >= nextAttackTime)
+        {
+            Fire();
+            nextAttackTime = Time.time + 1f / attackSpeed;
+        }
     }
-    private void Shoot()
+    public void Rotation()
     {
-
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, rotationYSlider.value, transform.rotation.eulerAngles.z);
+        transform.rotation = Quaternion.Euler(-rotationXSlider.value, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+    }
+    public void Fire()
+    {
+        // Instanciar el proyectil y aplicar fuerza
+        Rigidbody projectileInstance = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+        projectileInstance.AddForce(firePoint.forward * powerSlider.value, ForceMode.Impulse);
     }
 }
